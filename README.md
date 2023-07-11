09af1ab5558a4f321a3d1367510cd84022e6c66c
Ankush Agarwal <ankushagarwal11@gmail.com>
2023-07-11 16:23:05 -0700

    Update

--- README.md
+++ README.md
@@ -7,4 +7,73 @@ This project can take youtube videos or youtube playlists, and convert them into
 
 * yt-dlp (brew install yt-dlp)
 * python 3
-* pip install openai
+* pip install openai epub2txt
+* OPENAI_API_KEY as env variable
+  * export OPENAI_API_KEY="sk-xxxxxxxxxxxxxxxxxx"
+
+## Summarize youtube videos
+
+You can use the `summarize_yt_video.sh`. This uses yt-dlp to download the subtitles
+It cleans up the subtitles using a package called srt_fix. A copy of that package is here. It then calls text_file_summarizer.py to summarize the output
+
+Example Invocation
+
+```
+./summarize_yt_video.sh 'https://www.youtube.com/watch?v=zRtdikFQ-Pg'
+
+...
+Writing to outputs/Dr. Peter Attia — His Rules for Alcohol Consumption (How Much, When, and More).summary.txt
+...
+```
+
+## Summarize youtube playlists
+
+You can use the `summarize_yt_playlist.sh` script. This uses `yt-dlp` and the `summarize_yt_video.sh` script. It extracts all videos from
+the youtube playlist and calls `summarize_yt_video.sh` one by one
+
+
+
+
+Example Invocation
+
+```
+./summarize_yt_playlist.sh 'https://www.youtube.com/playlist?list=PL2D5A39CA456F09D7'
+
+...
+Summarizing 1/22 https://www.youtube.com/watch?v=M-129JLTjkQ...
+title = The Basics of Non Violent Communication 1.1
+...
+```
+
+## Summarize books
+
+You need to download the epub format of the book. You can use https://annas-archive.org/ to download it. See https://hn.algolia.com/?q=annas-archive.org or https://www.reddit.com/r/Annas_Archive/ for alternate sources
+
+
+### Step 1 Extract Chapters from the ebook
+
+We will use the `epub_book_to_chapters.py` script. This will create a json file.
+
+The json file is a list of "chapters" in the epub ebook. You need to manually remove the first few and the last few entries from the
+json file manually. This leaves you with a json file which only contains the actual chapters of the book.
+
+After cleaning up the book, save the json file.
+
+Example invocation
+
+```
+python epub_book_to_chapters.py --input_epub_file Outlive.epub
+
+<Manual Cleanup>
+```
+
+### Step 2 Summarize the book using the json file
+
+We will use the `chapters_json_to_summary.py` file to summarize the book. This reads the
+json file and goes over chapter by chapter and writes the summary to a single file per chapter. Internally, it uses text_file_summarizer.py.
+
+Example invocation
+
+```
+python chapters_json_to_summary.py --input_json_file outputs/Outlive.json
+```
--- text_file_summarizer.py
+++ text_file_summarizer.py
@@ -1,24 +1,24 @@
 import openai
 import os
-import sys
 import argparse
-
+import time
 # check if openai_key is set in environment variable
 if os.environ.get("OPENAI_API_KEY"):
-  openai.api_key = os.environ.get("OPENAI_API_KEY")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
 else:
-  # exit the program
-  print("Please set the OPENAI_API_KEY environment variable.")
-  exit()
+    # exit the program
+    print("Please set the OPENAI_API_KEY environment variable.")
+    exit()
 
 total_tokens_used = 0
+
 
 def summarize(content_title, text, content_type):
     global total_tokens_used
     system_content = f"""
 You are a brilliant content summarizer. You are given the following section from a content titled "{content_title}". The content type is {content_type}.
 Can you provide a comprehensive summary of the given content?
-The summary should cover all the key points and mainideas presented in the original text, while also condensing the information into a concise and easy-to-understand format.
+The summary should cover all the key points and main ideas presented in the original text, while also condensing the information into a concise and easy-to-understand format.
 Please ensure that the summary includes relevant details and examples that support the main ideas, while avoiding any unnecessary information or repetition.
 The length of the summary should be appropriate for the length and complexity of the original text, providing a clear and accurate overview without omitting any important information.
 The summary should be in a markdown list format using hyphens (-) as list delimiters.
@@ -28,75 +28,88 @@ The summary should be in a markdown list format using hyphens (-) as list delimi
     Here is the section within triple angle brackets : <<< {text} >>>
     Detailed Summary:
     """
+    attempts = 5
+    for attempt in range(attempts):
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": system_content},
+                    {"role": "user", "content": user_content}
+                ]
+            )
+            break
+        except Exception as e:
+            print(f"OpenAI ChatCompletion failed: {e}. Trying again.")
+            if attempt < attempts - 1:  # i.e., if it's not the last attempt
+                time.sleep(5)  # Wait for 5 seconds before next attempt
+                continue
+            else:
+                raise  # Re-raise the last exception if all attempts fail
 
-    response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {"role": "system", "content": system_content},
-            {"role": "user", "content": user_content}
-        ]
-    )
     total_tokens_used += response['usage']['total_tokens']
     return f"{response['choices'][0]['message']['content']}"
 
 
 def chunk_text(text, chunk_size_words, overlap_bw_chunks):
-  """
-  Splits a given text into chunks of a specified size, with a specified overlap between the chunks.
+    """
+    Splits a given text into chunks of a specified size, with a specified overlap between the chunks.
 
-  Parameters:
-    - text (str): The text to be chunked.
-    - chunk_size_words (int): The number of words in each chunk.
-    - overlap_bw_chunks (int): The number of words overlapping between each chunk.
-  """
-  words = text.split()
-  m = overlap_bw_chunks
-  n = chunk_size_words
-  chunked_words = [words[i:i+n] for i in range(0, len(words), n-m)]
-  return [" ".join(chunked_words[i]) for i in range(len(chunked_words))]
+    Parameters:
+      - text (str): The text to be chunked.
+      - chunk_size_words (int): The number of words in each chunk.
+      - overlap_bw_chunks (int): The number of words overlapping between each chunk.
+    """
+    words = text.split()
+    m = overlap_bw_chunks
+    n = chunk_size_words
+    chunked_words = [words[i:i+n] for i in range(0, len(words), n-m)]
+    return [" ".join(chunked_words[i]) for i in range(len(chunked_words))]
+
 
 def summarize_content(content_title, text, max_words, content_type, output_filename):
 
-  chunks = chunk_text(text, max_words, max_words // 10)
+    chunks = chunk_text(text, max_words, max_words // 10)
 
-  print(f"Split the text into {len(chunks)} chunks of {max_words} words.")
+    print(f"Split the text into {len(chunks)} chunks of {max_words} words.")
 
-  # open the output file_name and write the summary to it
-  with open(output_filename, 'w') as f:
-    f.write(f"AI Summary and Ideas List\n\n")
-    for i, chunk in enumerate(chunks):
-      print(f"Processing chunk {i+1}/{len(chunks)}")
-      f.write(f"Part {i+1}/{len(chunks)}:\n\n")
-      f.write(f"{summarize(content_title, chunk, content_type)}\n\n")
-
+    # open the output file_name and write the summary to it
+    print(f"Writing to {output_filename}")
+    with open(output_filename, 'w') as f:
+        for i, chunk in enumerate(chunks):
+            print(f"Processing chunk {i+1}/{len(chunks)}")
+            f.write(f"Part {i+1}/{len(chunks)}:\n\n")
+            f.write(f"{summarize(content_title, chunk, content_type)}\n\n")
 
 
 def get_title_from_file_path(filepath):
-  base = os.path.basename(filepath)
-  # Split the extension
-  return os.path.splitext(base)[0]
+    base = os.path.basename(filepath)
+    # Split the extension
+    return os.path.splitext(base)[0]
+
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--input_file', type=str)
-  parser.add_argument('--output_file', type=str)
-  parser.add_argument('--content_type', type=str, choices=['book_chapter', 'general_text', 'blog_post', 'podcast'])
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str)
+    parser.add_argument('--output_file', type=str)
+    parser.add_argument('--content_type', type=str,
+                        choices=['book_chapter', 'general_text', 'blog_post', 'podcast'])
+    args = parser.parse_args()
 
+    # check if the file exists and is valid
+    if not os.path.isfile(args.input_file):
+        print(f"File '{args.input_file}' does not exist or is not accessible.")
+        return
 
-  # check if the file exists and is valid
-  if not os.path.isfile(args.input_file):
-    print(f"File '{args.input_file}' does not exist or is not accessible.")
-    return
+    # read the file into a string
+    with open(args.input_file, 'r') as f:
+        text = f.read()
 
-  # read the file into a string
-  with open(args.input_file, 'r') as f:
-    text = f.read()
-
-  summarize_content(get_title_from_file_path(args.input_file), text, 1500, args.content_type, args.output_file)
-  print(f"Estimated cost = {round(total_tokens_used * 0.0015 * 0.001, 3)} USD")
-
+    summarize_content(get_title_from_file_path(args.input_file),
+                      text, 1500, args.content_type, args.output_file)
+    print(
+        f"Estimated cost = {round(total_tokens_used * 0.0015 * 0.001, 3)} USD")
 
 
 if __name__ == "__main__":
-  main()
+    main()
